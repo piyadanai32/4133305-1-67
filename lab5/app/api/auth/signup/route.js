f35b379bf82709592fe2c8f9b3afab49b8667fab
piyadanai32 <650112418059@bru.ac.th>
2024-07-29 19:47:53 +0700

    lab5

--- lab5/app/api/auth/signup/route.js
+++ lab5/app/api/auth/signup/route.js
@@ -1,27 +1,24 @@
 import { PrismaClient } from "@prisma/client";
-import { hash, hashSync } from "bcrypt";
+import { hashSync } from "bcrypt";
 
-const prisma = new PrismaClient ();
+const prisma = new PrismaClient();
 
-export async function POST (req){
-try {    
-    const { email, password, name } = await req.json();
-    // const hasheedPassword = await hash(password,10);
-    const hashedPassword = hashSync(password,10);
-
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
+    const hashedPass = hashSync(password, 10);
     const newUser = await prisma.user.create({
-        data: {
-            email,
-            name,
-            password: hashedPassword,
-        },
-    })
-
-    return Response.json({
-        msg: "User Created!",
-        newUser,
+      data: {
+        name,
+        email,
+        password: hashedPass,
+      },
     });
-    } catch (err){
-    return Response.json(err,{ status: 500});
-    }
+    return Response.json({
+      mgs: "user created!",
+      data: newUser,
+    });
+  } catch (err) {
+    return Response.json(err, { status: 500 });
+  }
 }
